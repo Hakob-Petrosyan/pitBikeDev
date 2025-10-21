06f929756724e2e1ab7b45a3e30409c334e5eda8
Hakob <hakob5@mail.ru>
2025-10-21 16:06:23 +0500

    start project

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import { readdirSync } from 'fs'
+import handlebars from 'vite-plugin-handlebars'
+
+// ищем все html файлы в корне
+const htmlFiles = readdirSync(__dirname).filter(f => f.endsWith('.html'))
+
+const input = htmlFiles.reduce((entries, file) => {
+    const name = file.replace('.html', '')
+    entries[name] = resolve(__dirname, file)
+    return entries
+}, {})
+
+
+export default defineConfig({
+    plugins: [
+        handlebars({
+            // Указываем несколько директорий с partials
+            partialDirectory: [
+                resolve(__dirname, 'src/components/common'),
+                resolve(__dirname, 'src/components/features')
+            ]
+        })
+    ],
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            input
+        }
+    },
+    server: {
+        open: '/index.html'
+    }
+})
+
+
